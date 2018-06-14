f18ed3623aeaa801946e4a127e41600e04daaf97
yarplux <yarplux@yandex.ru>
2018-06-14 21:45:17 +0300

    second version
    # 2.0 - все настройки в файле опций, код структурирован, где-то сокращены необязательные переменные
    # удалены методы по разнице кадров

--- VideoFilterParams.py
+++ VideoFilterParams.py
@@ -6,201 +6,153 @@
 # выделение нужного контура
 # выделение трека
 
-import numpy as np
+# 2.0 - все настройки в файле опций, код структурирован, где-то сокращены необязательные переменные
+# удалены методы по разнице кадров
+
 import cv2
-import os
 import utilFunctions as uF
 from enum import Enum
 
+import time
+import os
+import numpy as np
 
-class Out(Enum):
-    #source, resImg, ed, resDiff, edDiff, copy, resMark, edMark, resDiffMark, edDiffMark, track, settOrigin, settDiff, setGeneral = range(14)
-    #source, resImg, resDiff, copy, resMark, resDiffMark, track, settOrigin, settDiff, setGeneral = range(10)
-    #source, resImg, resDiff, copy, resMark, resDiffMark, track, settOrigin, setGeneral = range(9)
-    source, resImg, resDiff, copy, track, settOrigin, setGeneral = range(7)
+# ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ
+# ---------------------------------------------------------------------------------------------------------------
+
 
 def f(x): return x
 
 
-# Функция обработчика нажатия мыши
-def chooseObject(event, x, y, flags, param):
-    global coord, img, mouseDown
+def chooseObject(event, x, y, flags, param):                                                                            # Функция обработчика нажатия мыши
+
+    global cx, cy, img, mouseDown
     if event == cv2.EVENT_LBUTTONDOWN:
-        for i in coord:
-            i[0] = x
-            i[1] = y
+        cx = x
+        cy = y
         mouseDown = True
     if event == cv2.EVENT_LBUTTONUP:
         mouseDown = False
 
 
-numx = 0
-numy = 0
+# ИЦИНИАЛИЗАЦИЯ
+# ---------------------------------------------------------------------------------------------------------------
 
-x0 = 20
-y0 = 20
-w0 = 350
-h0 = 280
+print('Input filename:')
+filename = input()
+
+fOptions = open('options.txt', 'r')
+opt = {}
+topt = {}
+line = fOptions.readline()
+while line:
+    if line[0] != '#' and line != "\n":
+        line = line.split(' ')
+        line[1] = line[1].replace('\n','')
+        if line[1].isnumeric():
+            opt[line[0]] = int(line[1])
+        else:
+            topt[line[0]] = line[1]
+    line = fOptions.readline()
+fOptions.close()
+
+print(opt)
+print(topt)
 
 
+class Out(Enum):
+    source, resImg, copy, set, track = range(5)
 
-# 6 бегунков для настройки начального и конечного цвета фильтра
-hsvList = [0, 73, 185, 255, 97, 255]
-nameList = ['h1','s1','v1','h2','s2','v2']
 
-# 4 бегунка для настройки обрезки кадра + для задержки кадров
-coordList = [[16,120], [300,300], [30,120], [275,300], [1,1000]]
-nameCoordList = ['x0', 'width', 'y0', 'height', 'delay']
-
+numx, numy = 0, 0
 for win in Out:
-    xw = x0+numx*w0
-    #if xw > 1500:
-    if xw > 1000:
-        xw = x0
+    xw = opt['x0']+numx*opt['w0']
+    if "track" in win.name:
+        xw = opt['x0']
         numx = 0
         numy+=1
 
-    yw = y0+numy*h0
+    yw = opt['y0']+numy*opt['h0']
 
     cv2.namedWindow(win.name)
     cv2.setMouseCallback(win.name, chooseObject)
     cv2.moveWindow(win.name, xw, yw)
-    cv2.resizeWindow(win.name, w0-20, h0)
+    cv2.resizeWindow(win.name, opt['w0']-20, opt['h0'])
     numx += 1
 
-    if "sett" in win.name:
-        cv2.resizeWindow(win.name, 550, 300)
-        i = 0
-        for num in hsvList:
-            cv2.createTrackbar(nameList[i], win.name, num, 255, f)
-            i = i + 1
+    if "set" in win.name:
+        for n in ['x0', 'width', 'y0', 'height', 'delay']:                                                              # 4 бегунка для настройки обрезки кадра + для задержки кадров
+            cv2.createTrackbar(n, win.name, opt[n], opt[n+'_Max'], f)
 
-    if "General" in win.name:
-        i = 0
-        for n in coordList:
-            cv2.createTrackbar(nameCoordList[i], win.name, n[0], n[1], f)
-            i = i+1
+        for n in ['h1','h2','s1','s2','v1','v2']:                                                                       # 6 бегунков для настройки начального и конечного цвета фильтра
+            cv2.createTrackbar(n, win.name, opt[n], 255, f)
+
+        cv2.resizeWindow(win.name, 400, 250)
 
 
-# Создаём объект для захвата кадров из видео
-print('Input filename:')
-filename = input()
+# НАЧАЛО ВЫПОЛНЕНИЯ ОСНОВНОГО ЦИКЛА ПРОГРАММЫ
+# ---------------------------------------------------------------------------------------------------------------
 
 flExit = False
 mouseDown = False
+
 # Цикл прокрутки видео заново по кругу до выхода
 while not flExit:
 
-    delta = 3
-    diff = 0
-    imgs = [0 for i in range (3)]
-    indImg = 0
-    flag = False
-    flagFirst = True
-
-    # Вывод в цикле
-    # абсолютный cчётчик фреймов
-    counter = 0
-
-    # максимальный "скачок" точки отмечающей положение крысы за 1 кадр
-    maxjump = 100
-
-    path_color = (255, 0, 0)
-
     cap = cv2.VideoCapture(filename)
     ret, img = cap.read()
+
     results = [uF.initPath(img) for i in Out]
-    # создаём массив координат, по которым будет отслеживаться крыса для всех вариантов
-    # resImg, resDiffImg
-    # x y для каждой
-    coord = [[0, 0], [0, 0]]
+    counter = 0
+    cx = 0
+    cy = 0
+
+    length = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 
     while not flExit:
-        # Захват текущего кадра
         ret, results[Out.source.value] = cap.read()
         counter +=1
 
         # при достижении последнего фрейма - перезапуск видео
-		
-		#print(cap.get(cv2.CAP_PROP_POS_FRAMES))
-        if not ret:
+        if (not ret) and counter < length:
             break
-        #    cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
         try:
-            # Модификации кадра__________________________
-            # Обрезка копии кадра
-            results[Out.settOrigin.value] = uF.imgCut(results[Out.source.value], Out.setGeneral.name)
-            results[Out.copy.value] = results[Out.settOrigin.value].copy()
+            results[Out.set.value] = uF.imgCut(results[Out.source.value], Out.set.name)                                 # Обрезка кадра
+            results[Out.copy.value] = results[Out.set.value].copy()                                                     # Создание копии изображения
+            results[Out.resImg.value] = uF.imageFilter(results[Out.set.value], Out.set.name)                            # Наложение фильтров
 
-            # Захват разницы кадров
+            if counter == 1:                                                                                            # Для первого кадра - нажатие мышкой
+                trace = uF.initPath(results[Out.set.value])
+                last = [[cx, cy]]
+                cv2.imshow(Out.source.name, results[Out.source.value])
+                cv2.waitKey()
+                flagFirst = False
 
-            if counter%delta == 0:
-                imgs[indImg] = uF.imageFilter(results[Out.source.value], Out.settOrigin.name)
-                indImg = indImg + 1
-                if indImg == 3:
-                    results[Out.resDiff.value] = uF.diffImg(imgs[indImg - 3], imgs[indImg - 2], imgs[indImg - 1])
-                    imgs[indImg - 3] = imgs[indImg - 2]
-                    imgs[indImg - 2] = imgs[indImg - 1]
-                    indImg = 2
-                    flag = True
+            tempx, tempy, area = uF.findMaxCenter(results[Out.resImg.value])                                            # Поиск центра максимального контура в кадре
+            cx, cy = uF.circleIf([tempx, tempy, area], [cx, cy], (255, 0, 0), opt['maxjump'], results[Out.copy.value])  # Новое положение круга-метки центра
 
-            if counter == 1:
-                trace = uF.initPath(results[Out.settOrigin.value])
+            for img in Out:                                                                                             # отображение - перерисовка всех окон, кроме окна опций
+                if img.name != Out.set.name:
+                    cv2.imshow(img.name, results[img.value])
+            cv2.imshow(Out.track.name, trace)                                                                           # Дорисовка трека
 
-            if flag:
-                results[Out.resImg.value] = uF.imageFilter(results[Out.settOrigin.value], Out.settOrigin.name)
+            if not (cx*2 == last[len(last)-1][0] and cy*2 == last[len(last)-1][1]):                                     # строим линию основного трека, если текущие отвечают условиям
+                last.append([cx*2, cy*2])
+                cv2.line(trace, (last[len(last)-1][0], last[len(last)-1][1]), (last[len(last)-2][0], last[len(last)-2][1]), (opt['bT'],opt['gT'],opt['rT']), 1)
 
-                if flagFirst:
-                    # Для "первого" кадра - нажимается мышкой
-                    # coord[0][0], coord[0][1], area = uF.findMaxCenter(results[Out.resImg.value])
-                    # coord[1][0], coord[1][1], area = uF.findMaxCenter(results[Out.resDiff.value])
-                    last = [coord[0]]
-                    cv2.imshow(Out.source.name, results[Out.source.value])
-                    cv2.waitKey()
-                    flagFirst = False
-
-                #results[Out.resMark.value] = results[Out.resImg.value].copy()
-                tempx, tempy, area = uF.findMaxCenter(results[Out.resImg.value])
-                x, y = uF.circleIf([tempx, tempy, area], coord[0], (255, 0, 0), maxjump, results[Out.copy.value])
-                #uF.circleIf([tempx, tempy], coord, 0, maxjump, results[Out.resMark.value])
-
-                #results[Out.resDiffMark.value] = results[Out.resDiff.value].copy()
-                tempx, tempy, area = uF.findMaxCenter(results[Out.resDiff.value])
-                uF.circleIf([tempx, tempy, area], coord[1], (0, 0, 255), maxjump, results[Out.copy.value])
-                #uF.circleIf([tempx, tempy], coord, 1, maxjump, results[Out.resDiffMark.value])
-
-                # отображение
-                outList = [[i.name, i.value] for i in Out]
-                for i in range (0, Out.track.value):
-                    cv2.imshow(outList[i][0], results[outList[i][1]])
-                cv2.imshow(Out.track.name, trace)
-
-                #print (len(last)-1, x*2, last[len(last)-1][0], y*2, last[len(last)-1][1])
-                if not (x*2 == last[len(last)-1][0] and y*2 == last[len(last)-1][1]):
-                    last.append([x*2, y*2])
-                    cv2.line(trace, (last[len(last)-1][0], last[len(last)-1][1]), (last[len(last)-2][0], last[len(last)-2][1]), path_color, 1)
         except:
             cap.release()
             raise
 
-        s = cv2.getTrackbarPos('delay',Out.setGeneral.name)
-        ch = cv2.waitKey(s)
-        if ch == 32:
-            ch = cv2.waitKey()
-            if (ch == 13):
-                #os.mkdir('temptrack')
-                #cv2.imwrite('./img.png', results[Out.source.value])
-                cv2.imwrite('./resImg.png', results[Out.resImg.value])
-                break
-        if ch == 27:
-            flExit = True
+        cv2.waitKey(cv2.getTrackbarPos('delay', Out.set.name))
 
     uF.analizeTrace(last, 30)
-    track = uF.printTrace(trace, last, (255, 255, 255))
-    cv2.imshow('Track', track)
-    cv2.imwrite('./track.png', track)
+    track = uF.printTrace(trace, last, (opt['bL'], opt['gL'], opt['rL']))
+    cv2.imshow(Out.track.name, track)
+    cv2.imwrite(topt['trackFile'], track)
     cv2.waitKey()
+    flExit = True
     cap.release()
 
 cv2.destroyAllWindows()
